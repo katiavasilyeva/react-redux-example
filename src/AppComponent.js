54306c740a061a207f9bed158548fbd1f2e21ae7
Katia Vasilyeva <vasilyeva64@gmail.com>
2018-07-18 15:31:00 -0400

    testing username

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,4 +1,4 @@
 import React from 'react';
 
 
-export const AppComponent = (props) => <React.Fragment> <p>{props.yoBool? props.showSmiley : "HAHA"} </p> <button onClick={props.onClick}></button> </React.Fragment>;
+export const AppComponent = (props) => <React.Fragment> <p>{props.yoBool? props.showSmiley : "HAHAH"} </p> <button onClick={props.onClick}></button> </React.Fragment>;
